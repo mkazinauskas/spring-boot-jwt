03bfa419466ceba749762814408bc5d56554b450
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-02-19 23:41:28 +0200

    Unused class removal

--- src/main/groovy/com/modzo/jwt/JwtApplication.groovy
+++ src/main/groovy/com/modzo/jwt/JwtApplication.groovy
@@ -1,12 +1,8 @@
 package com.modzo.jwt
 
-import com.modzo.jwt.resources.admin.users.UserActionService
-import com.modzo.jwt.resources.admin.users.register.RegisterUserRequest
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Primary
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -24,16 +20,4 @@ class JwtApplication {
         return new BCryptPasswordEncoder()
     }
 
-    @Bean
-    CommandLineRunner init(UserActionService accountService) {
-        return {
-            ['admin@admin.com', 'user@user.com'].each
-                    { username ->
-                        RegisterUserRequest acct = new RegisterUserRequest()
-                        acct.setEmail(username)
-                        acct.setPassword("password")
-                        accountService.registerUser(acct)
-                    }
-        }
-    }
 }
