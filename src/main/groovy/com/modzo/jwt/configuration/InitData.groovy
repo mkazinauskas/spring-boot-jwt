8ac2dc2f2d61b5112c983455e18410c5905ce6a2
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-02-20 22:55:05 +0200

    Init data implementation

---
+++ src/main/groovy/com/modzo/jwt/configuration/InitData.groovy
@@ -0,0 +1,57 @@
+package com.modzo.jwt.configuration
+
+import com.modzo.jwt.configuration.init.DataInitService
+import com.modzo.jwt.domain.clients.Clients
+import com.modzo.jwt.domain.users.Users
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+
+import static com.modzo.jwt.domain.users.User.Authority.*
+
+@Component
+@ConditionalOnProperty(name = 'application.init.data', havingValue = 'true')
+class InitData {
+
+    private static final DataInitService.TestClient CLIENT = new DataInitService.TestClient('testClient', 'testSecret')
+
+    private static final DataInitService.TestUser ADMIN_USER = new DataInitService.TestUser(
+            email: 'admin@gmail.com',
+            password: 'adminPassword',
+            authorities: [REGISTERED_USER, ADMIN] as Set
+    )
+    private static final DataInitService.TestUser SIMPLE_USER = new DataInitService.TestUser(
+            email: 'user@gmail.com',
+            password: 'userPassword',
+            authorities: [REGISTERED_USER, USER] as Set
+    )
+    private static final DataInitService.TestUser SIMPLE_REGISTERED_USER = new DataInitService.TestUser(
+            email: 'registereduser@gmail.com',
+            password: 'registeredUserPassword',
+            authorities: [REGISTERED_USER] as Set
+    )
+
+    @Bean
+    InitializingBean initTestClients(
+            Clients clients,
+            DataInitService dataInitService) {
+        return {
+            if (clients.count() == 0) {
+                dataInitService.createClient(CLIENT)
+            }
+        }
+    }
+
+    @Bean
+    InitializingBean initTestUsers(Users users, DataInitService dataInitService) {
+        return {
+            if (users.count() == 0) {
+                dataInitService.createUser(ADMIN_USER)
+                dataInitService.createUser(SIMPLE_USER)
+                dataInitService.createUser(SIMPLE_REGISTERED_USER)
+            }
+        }
+    }
+
+}
--- src/test/groovy/com/modzo/jwt/init/TestDataInit.groovy
+++ src/test/groovy/com/modzo/jwt/init/TestDataInit.groovy
@@ -1,114 +1,46 @@
 package com.modzo.jwt.init
 
-import com.modzo.jwt.domain.clients.Client
-import com.modzo.jwt.domain.clients.commands.CreateClient
-import com.modzo.jwt.domain.clients.commands.UpdateClientData
-import com.modzo.jwt.domain.users.User
-import com.modzo.jwt.domain.users.commands.CreateUser
-import com.modzo.jwt.domain.users.commands.UpdateUserData
-import groovy.transform.Immutable
+import com.modzo.jwt.configuration.init.DataInitService
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 
-import static com.modzo.jwt.domain.users.User.Authority.REGISTERED_USER
-import static com.modzo.jwt.domain.users.User.Authority.ADMIN
-import static com.modzo.jwt.domain.users.User.Authority.USER
+import static com.modzo.jwt.domain.users.User.Authority.*
 import static com.modzo.jwt.helpers.RandomDataUtil.randomEmail
 
 @Component
 class TestDataInit {
 
-    static final TestClient TEST_CLIENT = new TestClient('test', 'secret')
-    static final TestUser TEST_ADMIN_USER = new TestUser(
+    static final DataInitService.TestClient TEST_CLIENT = new DataInitService.TestClient('test', 'secret')
+    static final DataInitService.TestUser TEST_ADMIN_USER = new DataInitService.TestUser(
             email: randomEmail(),
             password: 'adminPassword',
             authorities: [ADMIN] as Set
     )
-    static final TestUser TEST_USER = new TestUser(
+    static final DataInitService.TestUser TEST_USER = new DataInitService.TestUser(
             email: randomEmail(),
             password: 'userPassword',
             authorities: [USER] as Set
     )
-    static final TestUser TEST_REGISTERED_USER = new TestUser(
+    static final DataInitService.TestUser TEST_REGISTERED_USER = new DataInitService.TestUser(
             email: randomEmail(),
             password: 'registeredUserPassword',
             authorities: [REGISTERED_USER] as Set
     )
 
     @Bean
-    InitializingBean initTestClients(CreateClient.Handler createClientHandler,
-                                     UpdateClientData.Handler updateClientDataHandler) {
+    InitializingBean initTestClients(DataInitService dataInitService) {
         return {
-            createClient(createClientHandler, updateClientDataHandler, TEST_CLIENT)
+            dataInitService.createClient(TEST_CLIENT)
         }
-    }
-
-    private static void createClient(CreateClient.Handler createClientHandler,
-                                     UpdateClientData.Handler updateClientDataHandler,
-                                     TestClient testClient) {
-        CreateClient.Response response = createClientHandler.handle(new CreateClient(
-                testClient.clientId,
-                testClient.secret)
-        )
-        updateClientDataHandler.handle(new UpdateClientData(
-                uniqueId: response.uniqueId,
-                clientId: testClient.clientId,
-                scoped: true,
-                secretRequired: true,
-                enabled: true,
-                autoApprove: true,
-                accessTokenValiditySeconds: 3600,
-                refreshTokenValiditySeconds: 36000,
-                authorities: [Client.Authority.CLIENT],
-                scopes: [Client.Scope.READ, Client.Scope.WRITE],
-                authorizedGrantTypes: [Client.GrantType.PASSWORD, Client.GrantType.REFRESH_TOKEN],
-                registeredRedirectUris: ['http://google.com'],
-                resourceIds: []
-        ))
     }
 
     @Bean
-    InitializingBean initTestUsers(CreateUser.Handler createUserHandler,
-                                   UpdateUserData.Handler updateUserDataHandler) {
+    InitializingBean initTestUsers(DataInitService dataInitService) {
         return {
-            createUser(createUserHandler, updateUserDataHandler, TEST_ADMIN_USER)
-            createUser(createUserHandler, updateUserDataHandler, TEST_USER)
-            createUser(createUserHandler, updateUserDataHandler, TEST_REGISTERED_USER)
-        }
-    }
-
-    private static void createUser(CreateUser.Handler createUserHandler,
-                                   UpdateUserData.Handler updateUserDataHandler,
-                                   TestUser testUser) {
-        CreateUser.Response response = createUserHandler.handle(testUser.asCreateUserCommand())
-        updateUserDataHandler.handle(new UpdateUserData(
-                uniqueId: response.uniqueId,
-                email: testUser.email,
-                enabled: true,
-                accountNotExpired: true,
-                credentialsNonExpired: true,
-                accountNotLocked: true,
-                authorities: testUser.authorities
-        ))
-    }
-
-    @Immutable
-    static class TestClient {
-        String clientId
-        String secret
-    }
-
-    @Immutable
-    static class TestUser {
-        String email
-        String password
-        Set<User.Authority> authorities = []
-
-        CreateUser asCreateUserCommand() {
-            return new CreateUser(
-                    email, password
-            )
+            dataInitService.createUser(TEST_ADMIN_USER)
+            dataInitService.createUser(TEST_USER)
+            dataInitService.createUser(TEST_REGISTERED_USER)
         }
     }
 }
