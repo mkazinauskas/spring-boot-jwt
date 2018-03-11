5b7ce8fb0890a3104fe475bdc87650a552c5e5fb
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-03-11 21:24:01 +0200

    Spring upgrade

--- src/main/groovy/com/modzo/jwt/server/security/WebSecurityConfig.java
+++ src/main/groovy/com/modzo/jwt/server/security/WebSecurityConfig.java
@@ -2,19 +2,15 @@ package com.modzo.jwt.server.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.springframework.http.HttpMethod.DELETE;
-
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@Order(SecurityProperties.BASIC_AUTH_ORDER - 2)
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
