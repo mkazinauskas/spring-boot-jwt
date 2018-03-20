b4c5591ed146a68fb00573e91c8621756bb53077
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-03-20 20:42:36 +0200

    Index page improvements

--- src/test/groovy/com/modzo/jwt/resources/IndexPageIsAvailable.groovy
+++ src/test/groovy/com/modzo/jwt/resources/IndexPageIsAvailable.groovy
@@ -4,13 +4,18 @@ import com.modzo.jwt.AbstractSpec
 import org.springframework.http.ResponseEntity
 
 import static com.modzo.jwt.Urls.index
+import static org.springframework.http.HttpStatus.FOUND
 import static org.springframework.http.HttpStatus.OK
 
 class IndexPageIsAvailable extends AbstractSpec {
 
-    void 'index page should be accessible publicly'() {
+    void 'index page should redirect to swagger ui'() {
         when:
-            ResponseEntity<String> response = restTemplate.getForEntity(index(), String)
+            ResponseEntity<String> redirectEndpoint = restTemplate.getForEntity(index(), String)
+        then:
+            redirectEndpoint.statusCode == FOUND
+        when:
+            ResponseEntity<String> response = restTemplate.getForEntity(redirectEndpoint.headers.getLocation(), String)
         then:
             response.statusCode == OK
             response.body.contains('<title>Swagger UI</title>')
