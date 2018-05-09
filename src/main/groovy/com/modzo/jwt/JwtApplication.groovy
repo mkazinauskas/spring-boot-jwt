71dbf1d810314aa1db46848462ffa64fcc663cf7
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-05-09 15:55:52 +0300

    No async

--- src/main/groovy/com/modzo/jwt/JwtApplication.groovy
+++ src/main/groovy/com/modzo/jwt/JwtApplication.groovy
@@ -3,11 +3,9 @@ package com.modzo.jwt
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
-@EnableAsync
 @SpringBootApplication
 class JwtApplication {
 
